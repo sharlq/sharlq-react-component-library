bc7de8e852839cf1b43a440e76768748bc9182bc
sharlq <shehabakhader@gmail.com>
2021-05-07 18:47:35 +0300

    done some stylings and add features

--- src/select/select.jsx
+++ src/select/select.jsx
@@ -1,7 +1,10 @@
 import React,{useState} from 'react'
 import styles from "./select.module.css"
 const error =()=> {throw new Error("add a function that activates when pressing list item")}
-const Select = ({items=["firstItem","secondItem","thirdItem"],onItem = error}) => {
+const Select = ({
+    items=["firstItem","secondItem","thirdItem"],
+    onItem = error,
+    width = "175px"}) => {
 
         
         
@@ -22,13 +25,15 @@ const Select = ({items=["firstItem","secondItem","thirdItem"],onItem = error}) =
                 tabIndex="0">
 
         <div  className={styles.dropDownList} 
+                style={{width:width}}
         onClick={()=>handleOpenCategoriesList()}>
         <p>Categoryes</p> 
         <svg height="20px" viewBox="0 0 320 512">
             <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path></svg>
         </div>
 
-        {open && <div className={styles.dropDownListItems}>{categoriesItems}</div> }
+        {open && <div className={styles.dropDownListItems}
+        style={{width:width}}>{categoriesItems}</div> }
         </div>
         </div>
     )
