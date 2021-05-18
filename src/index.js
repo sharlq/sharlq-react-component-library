b8c283793d47c82086618a46061b64feac52d1bb
sharlq <shehabakhader@gmail.com>
2021-05-18 07:28:27 +0300

    fix smth

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ export * from "./button/Button"
 
 
 export * from './select/select'
-export * from './tablr/Tablr'
 export * from './card/Card'
 export * from './card/CardBody'
 export * from './card/CardImg'
